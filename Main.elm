6fbe1de9bfb53f7be7e4a3e11b8636ab714964f4
victor hazbun <victorhazbun87@gmail.com>
2018-03-27 03:33:41 -0500

    Use `Html.program` to Set up an Interactive Elm Application

--- Main.elm
+++ Main.elm
@@ -4,8 +4,42 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 
 
-main : Html a
+type alias Model =
+    { email : String
+    , message : String
+    }
+
+
+initialModel : Model
+initialModel =
+    { email = ""
+    , message = ""
+    }
+
+
+type Msg
+    = InputEmail
+    | InputMessage
+    | Submit
+
+
+main : Program Never Model Msg
 main =
+    program
+        { init = ( initialModel, Cmd.none )
+        , update = update
+        , subscriptions = \model -> Sub.none
+        , view = view
+        }
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
     Html.form []
         [ header
         , body
